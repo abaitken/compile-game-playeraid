add944d2bfadac1ec0a05175cf1689733f0bf9fb
Alex Boyne-Aitken <abaitken@live.co.uk>
2026-02-09 15:01:58 +0000

    feat: Applied markdown styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 import { ViewModel } from './ViewModel.js';
+import { marked } from '../node_modules/marked/lib/marked.esm.js';
+
+ko.bindingHandlers.markdown = {
+    update: function (element, valueAccessor) {
+        const value = ko.unwrap(valueAccessor());
+        const markup = marked(value)
+        element.innerHTML = markup;
+    }
+};
 
 var root = new ViewModel();
 root.init();
