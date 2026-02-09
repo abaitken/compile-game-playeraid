2fe4fac0b070cccf6628d8c2320a205352938a9e
Alex Boyne-Aitken <abaitken@live.co.uk>
2026-02-09 14:40:21 +0000

    feat: Added basic display of all cards

--- src/js/ViewModel.js
+++ src/js/ViewModel.js
@@ -1,6 +1,33 @@
 export class ViewModel {
+    constructor() {
+        this.data = ko.observable(null);
+        this.protocols = ko.computed(function(){
+            const data = this.data();
+
+            if(!data) {
+                return [];
+            }
+
+            return data.protocols;
+        }, this);
+    }
+
     init() {
         ko.applyBindings(this);
+        
+        fetch('data.json')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            this.data(data);
+        })
+        .catch(err => {
+            console.error('Failed to load JSON:', err);
+        });
     }
 }
 
