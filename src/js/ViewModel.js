5f3555d47ee1f71b151a11b52d7832fe4cbd3e84
Alex Boyne-Aitken <abaitken@live.co.uk>
2026-02-09 22:03:26 +0000

    refactor: Extracted class into seperate file

--- src/js/ViewModel.js
+++ src/js/ViewModel.js
@@ -1,51 +1,5 @@
 import { ProtocolModel } from "./ProtocolModel.js";
-
-class SelectionHandler {
-    constructor() {
-        this.mySelection = ko.observableArray();
-        this.opSelection = ko.observableArray();
-        this.excluded = ko.observableArray();
-        this.included = ko.pureComputed(function () {
-            return this.mySelection().concat(this.opSelection());
-        }, this);
-        this.selection = ko.pureComputed(function () {
-            return this.included().concat(this.excluded());
-        }, this);
-    }
-
-    includes(item) {
-        return this.selection().includes(item);
-    }
-
-    selectForMe(item) {
-        if (!this.includes(item) && this.mySelection().length < 3) {
-            this.mySelection.push(item);
-        } else {
-            this.mySelection.remove(item);
-        }
-    }
-
-    selectForOpponent(item) {
-        if (!this.includes(item) && this.opSelection().length < 3) {
-            this.opSelection.push(item);
-        } else {
-            this.opSelection.remove(item);
-        }
-    }
-
-    selectExclude(item) {
-        if (!this.includes(item)) {
-            this.excluded.push(item);
-        } else {
-            this.excluded.remove(item);
-        }
-    }
-
-    clear() {
-        this.mySelection.removeAll();
-        this.opSelection.removeAll();
-    }
-}
+import { SelectionHandler } from "./SelectionHandler.js";
 
 export class ViewModel {
     constructor() {
